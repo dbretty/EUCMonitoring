75a4b4c43d2af6afc3bebf477b3cdc39d92582e4
Adam Yarborough <adam.yarborough@gmail.com>
2018-07-12 07:28:23 -0400

    Target uninstall-visualizationsetup
    Remove only the Grafana, Influx, and NSSM directories from $MonitoringPath, as opposed to blowing away the entire $MonitoringPath.

--- PSGallery/Private/Uninstall-VisualizationSetup.ps1
+++ PSGallery/Private/Uninstall-VisualizationSetup.ps1
@@ -19,6 +19,8 @@ function Uninstall-VisualizationSetup {
         Name                    Version         Date                Change Detail
         Hal Lange               1.0             16/04/2018          Initial Creation of Installer
         Adam Yarborough         1.1             11/07/2018          Integration of Hal's work and updating.
+        Adam Yarborough         1.2             12/07/2018          Remove only Grafana, Influx, and NSSM 
+                                                                    items from $MonitoringPath
     .PARAMETER MonitoringPath
         Folder path to download files needed for monitoring process
     .EXAMPLE
@@ -36,17 +38,24 @@ function Uninstall-VisualizationSetup {
     process {
 
         #Removing Services
+        $Grafana = (get-childitem $MonitoringPath | Where-Object {$_.Name -match 'graf'}).FullName
+        $Influx = (get-childitem $MonitoringPath | Where-Object {$_.Name -match 'infl'}).FullName
         $NSSM = (get-childitem $MonitoringPath | Where-Object {$_.Name -match 'nssm'}).FullName
+
         $NSSMEXE = "$nssm\win64\nssm.exe"
         #Remove Grafana Service
+        Write-Output "Removing Grafana Server service"
         & $nssmexe Stop "Grafana Server"
         & $nssmexe Remove "Grafana Server" confirm
         #Remove Influx Service
+        Write-Output "Removing InfluxDB Server service"
         & $nssmexe Stop "InfluxDB Server"
         & $nssmexe Remove "InfluxDB Server" confirm
 
-        #Remove Directories, all of them.  Scorched earth.
-        Remove-Item -path $MonitoringPath -Recurse
+        #Remove service Directories, all of them.  Scorched earth.
+        Remove-Item -path $Grafana -Recurse
+        Remove-Item -path $Influx -Recurse
+        Remove-Item -path $NSSM -Recurse
 
         #Remove Variable
         Remove-Item Env:\Home
